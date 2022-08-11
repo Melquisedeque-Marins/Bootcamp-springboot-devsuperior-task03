b58e72b0bd7faec42ddfb21491713915c02c3d32
melck <melck_junior@hotmail.com>
2022-08-11 15:59:02 +0100

    Resource server

---
+++ src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -0,0 +1,46 @@
+package com.devsuperior.bds04.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
+
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
+    private static final String[] PUBLIC_GET= { "/cities/**", "/events**" };
+
+    private static final String[] CLIENT_OR_ADMIN = { "/events**"};
+
+    private static final String[] PsUBLIC = { "/oauth/token", "/h2-console/**", "/cities/**", "/events**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_GET).permitAll()
+                .antMatchers(HttpMethod.POST, CLIENT_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
+                .anyRequest().hasRole("ADMIN");
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO tb_user(email, password) VALUES ('ana@gmail.com', '123456');
 INSERT INTO tb_user(email, password) VALUES ('bob@gmail.com', '123456');
 
-INSERT INTO tb_role(authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role(authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role(authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
